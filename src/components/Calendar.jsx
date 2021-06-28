9a815dcfafd5e0a5cc2deb1917376f952adc9b5d
mantalbornoz <mantalbornoz@sodimac.cl>
2021-06-28 15:36:31 -0400

    se agregan mejoras

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import Config from '../utils/Config';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 
 
@@ -10,11 +11,26 @@ function Calendar() {
   const [date, setDate] = useState();
 
   useEffect(()=>{
+    console.log(`${`${value.getFullYear()}-${value.getMonth()+1 < 10 ? `0${value.getMonth()+1}` : value.getMonth()+1 }-${value.getUTCDate()}`} desde el useEffect inicial`);
+    const enviarDate = async () =>{
+        try {
+          const res = await axios(`${Config.urlBase}ok/${`${value.getFullYear()}-${value.getMonth()+1 < 10 ? `0${value.getMonth()+1}` : value.getMonth()+1 }-${value.getUTCDate()}`}`);
+          console.log(`respuesta desde reservas ok`,res.data.total);
+        } catch (error) {
+          if(error.status !== 401){
+            console.log(`Error al enviar la fecha`);
+          }
+        }
+    } 
+    enviarDate();
+  }, [])
+
+  useEffect(()=>{
     console.log(`${date} desde el useEffect`);
     const enviarDate = async () =>{
         try {
           const res = await axios(`${Config.urlBase}ok/${date}`);
-          console.log(res.data.date);
+          console.log(res.data.total);
         } catch (error) {
           if(error.status !== 401){
             console.log(`Error al enviar la fecha`);
@@ -46,7 +62,7 @@ function Calendar() {
             type="date"
             name="fechacita"
             min="2010-02-20"
-            max={`${value.getFullYear()}-${value.getMonth()+1 < 10 ? `0${value.getMonth()+1}` : value.getMonth()+1 }-${value.getUTCDate() -1 }`}
+            max={`${value.getFullYear()}-${value.getMonth()+1 < 10 ? `0${value.getMonth()+1}` : value.getMonth()+1 }-${value.getUTCDate()}`}
             onChange={handleChange}
             className="validate"
           />
--- src/components/Diagrams.jsx
+++ src/components/Diagrams.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useContext } from "react";
-import { Spinner } from "react-bootstrap";
+// import { Spinner } from "react-bootstrap";
 import ReactFlow from "react-flow-renderer";
 import { TotalContext } from "../context/TotalContext";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { StockContext } from "../context/StockContext";
+import Loader from "react-loader-spinner";
+
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 
 
 
@@ -23,27 +27,82 @@ export const Diagrams = () => {
 
   const elements = [
     {
-      id: 'horizontal-1',
-      sourcePosition: 'right',
+      id: "horizontal-1",
+      sourcePosition: "right",
       // type: 'input',
-      className: 'dark-node',
-      data: { label: <>Reservas confirmadas <br />{totalcount ? totalcount :<Spinner animation="border" variant="primary" />}</> },
+      className: "dark-node",
+      data: {
+        label: (
+          <>
+            Reservas confirmadas <br />
+            {totalcount ? (
+              totalcount
+            ) : (
+              <Loader
+                type="Puff"
+                color="#00BFFF"
+                height={25}
+                width={25}
+                timeout={40000} //3 secs
+              />
+            )}
+          </>
+        ),
+      },
       position: { x: 30, y: 180 },
-      draggable: false
+      draggable: false,
     },
     {
-      id: 'horizontal-2',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: <>Reservas ok <br /> <p>{reservasok ? reservasok : <Spinner animation="border" variant="secondary" />}</p></>},
+      id: "horizontal-2",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: {
+        label: (
+          <>
+            Reservas ok <br />{" "}
+            <p>
+              {reservasok ? (
+                reservasok
+              ) : (
+                <Loader
+                  type="Puff"
+                  color="#00BFFF"
+                  height={25}
+                  width={25}
+                  timeout={40000} //3 secs
+                />
+              )}
+            </p>
+          </>
+        ),
+      },
       position: { x: 280, y: 100 },
-      draggable: false
+      draggable: false,
     },
     {
-      id: 'horizontal-3',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: <>Reservas Pendientes <p>{reservaspendientes ? reservaspendientes : <Spinner animation="border" variant="danger" />}</p></> },
+      id: "horizontal-3",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: {
+        label: (
+          <>
+            Reservas Pendientes{" "}
+            <p>
+              {reservaspendientes ? (
+                reservaspendientes
+              ) : (
+                <Loader
+                  type="Puff"
+                  color="#00BFFF"
+                  height={25}
+                  width={25}
+                  timeout={40000} //3 secs
+                />
+              )}
+            </p>
+          </>
+        ),
+      },
       position: { x: 280, y: 260 },
     },
     // {
@@ -54,95 +113,171 @@ export const Diagrams = () => {
     //   position: { x: 700, y: 100 },
     // },
     {
-      id: 'horizontal-5',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: <>Pendiente Bodega<p>{pendientesproveedor? pendientesproveedor : <Spinner animation="border" variant="dark" />}</p></> },
+      id: "horizontal-5",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: {
+        label: (
+          <>
+            Pendiente Bodega
+            <p>
+              {pendientesproveedor ? (
+                pendientesproveedor
+              ) : (
+                <Loader
+                  type="Puff"
+                  color="#00BFFF"
+                  height={25}
+                  width={25}
+                  timeout={40000} //3 secs
+                />
+              )}
+            </p>
+          </>
+        ),
+      },
       position: { x: 550, y: 200 },
     },
     {
-      id: 'horizontal-6',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: <>Pendiente Provedor<p>{prendientesbodegas ? prendientesbodegas:<Spinner animation="border" variant="info" />}</p></> },
+      id: "horizontal-6",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: {
+        label: (
+          <>
+            Pendiente Provedor
+            <p>
+              {prendientesbodegas ? (
+                prendientesbodegas
+              ) : (
+                <Loader
+                  type="Puff"
+                  color="#00BFFF"
+                  height={25}
+                  width={25}
+                  timeout={40000} //3 secs
+                />
+              )}
+            </p>
+          </>
+        ),
+      },
       position: { x: 550, y: 330 },
     },
     {
-      id: 'horizontal-7',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: 'Node 7' },
+      id: "horizontal-7",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: { label: "Node 7" },
       position: { x: 900, y: 150 },
     },
     {
-      id: 'horizontal-8',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: '8' },
+      id: "horizontal-8",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: { label: "8" },
       position: { x: 1200, y: 270 },
     },
     {
-      id: 'horizontal-9',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: '9' },
+      id: "horizontal-9",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: { label: "9" },
       position: { x: 1200, y: 330 },
     },
     {
-      id: 'horizontal-19',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: <>Reservas rescatadas <p>{reservasresctadas? reservasresctadas :   <Spinner animation="border" variant="danger" />}</p></> },
+      id: "horizontal-19",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: {
+        label: (
+          <>
+            Reservas rescatadas{" "}
+            <p>
+              {reservasresctadas ? (
+                reservasresctadas
+              ) : (
+                <Loader
+                  type="Puff"
+                  color="#00BFFF"
+                  height={25}
+                  width={25}
+                  timeout={40000} //3 secs
+                />
+              )}
+            </p>
+          </>
+        ),
+      },
       position: { x: 900, y: 300 },
     },
     {
-      id: 'horizontal-20',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: <>Reservas informadas como pendientes <p>{reservasinfopendientes ? reservasinfopendientes : <Spinner animation="border" variant="warning" />}</p></> },
+      id: "horizontal-20",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: {
+        label: (
+          <>
+            Reservas informadas como pendientes{" "}
+            <p>
+              {reservasinfopendientes ? (
+                reservasinfopendientes
+              ) : (
+                <Loader
+                  type="Puff"
+                  color="#00BFFF"
+                  height={20}
+                  width={20}
+                  timeout={40000} //3 secs
+                />
+              )}
+            </p>
+          </>
+        ),
+      },
       position: { x: 900, y: 400 },
     },
     {
-      id: 'horizontal-21',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: '21' },
+      id: "horizontal-21",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: { label: "21" },
       position: { x: 1200, y: 400 },
     },
     {
-      id: 'horizontal-22',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: '22' },
+      id: "horizontal-22",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: { label: "22" },
       position: { x: 1200, y: 450 },
     },
     {
-      id: 'horizontal-23',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: '23' },
+      id: "horizontal-23",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: { label: "23" },
       position: { x: 1500, y: 380 },
     },
     {
-      id: 'horizontal-24',
-      sourcePosition: 'right',
-      targetPosition: 'left',
-      data: { label: '24' },
+      id: "horizontal-24",
+      sourcePosition: "right",
+      targetPosition: "left",
+      data: { label: "24" },
       position: { x: 1500, y: 450 },
     },
-  
+
     {
-      id: 'horizontal-e1-2',
-      source: 'horizontal-1',
-      type: 'smoothstep',
-      target: 'horizontal-2',
+      id: "horizontal-e1-2",
+      source: "horizontal-1",
+      type: "smoothstep",
+      target: "horizontal-2",
       animated: true,
     },
     {
-      id: 'horizontal-e1-3',
-      source: 'horizontal-1',
-      type: 'smoothstep',
-      target: 'horizontal-3',
+      id: "horizontal-e1-3",
+      source: "horizontal-1",
+      type: "smoothstep",
+      target: "horizontal-3",
       animated: true,
     },
     // {
@@ -153,80 +288,80 @@ export const Diagrams = () => {
     //   label: 'edge label',
     // },
     {
-      id: 'horizontal-e3-5',
-      source: 'horizontal-3',
-      type: 'smoothstep',
-      target: 'horizontal-5',
+      id: "horizontal-e3-5",
+      source: "horizontal-3",
+      type: "smoothstep",
+      target: "horizontal-5",
       animated: true,
     },
     {
-      id: 'horizontal-e3-6',
-      source: 'horizontal-3',
-      type: 'smoothstep',
-      target: 'horizontal-6',
+      id: "horizontal-e3-6",
+      source: "horizontal-3",
+      type: "smoothstep",
+      target: "horizontal-6",
       animated: true,
     },
     {
-      id: 'horizontal-e5-7',
-      source: 'horizontal-5',
-      type: 'smoothstep',
-      target: 'horizontal-7',
+      id: "horizontal-e5-7",
+      source: "horizontal-5",
+      type: "smoothstep",
+      target: "horizontal-7",
       animated: true,
     },
     {
-      id: 'horizontal-e19-8',
-      source: 'horizontal-19',
-      type: 'smoothstep',
-      target: 'horizontal-8',
+      id: "horizontal-e19-8",
+      source: "horizontal-19",
+      type: "smoothstep",
+      target: "horizontal-8",
       animated: true,
     },
     {
-      id: 'horizontal-e19-9',
-      source: 'horizontal-19',
-      type: 'smoothstep',
-      target: 'horizontal-9',
+      id: "horizontal-e19-9",
+      source: "horizontal-19",
+      type: "smoothstep",
+      target: "horizontal-9",
       animated: true,
     },
     {
-      id: 'horizontal-e20-21',
-      source: 'horizontal-20',
-      type: 'smoothstep',
-      target: 'horizontal-21',
+      id: "horizontal-e20-21",
+      source: "horizontal-20",
+      type: "smoothstep",
+      target: "horizontal-21",
       animated: true,
     },
     {
-      id: 'horizontal-e20-22',
-      source: 'horizontal-20',
-      type: 'smoothstep',
-      target: 'horizontal-22',
+      id: "horizontal-e20-22",
+      source: "horizontal-20",
+      type: "smoothstep",
+      target: "horizontal-22",
       animated: true,
     },
     {
-      id: 'horizontal-e22-23',
-      source: 'horizontal-22',
-      type: 'smoothstep',
-      target: 'horizontal-23',
+      id: "horizontal-e22-23",
+      source: "horizontal-22",
+      type: "smoothstep",
+      target: "horizontal-23",
       animated: true,
     },
     {
-      id: 'horizontal-e22-24',
-      source: 'horizontal-22',
-      type: 'smoothstep',
-      target: 'horizontal-24',
+      id: "horizontal-e22-24",
+      source: "horizontal-22",
+      type: "smoothstep",
+      target: "horizontal-24",
       animated: true,
     },
     {
-      id: 'horizontal-e6-20',
-      source: 'horizontal-6',
-      type: 'smoothstep',
-      target: 'horizontal-20',
+      id: "horizontal-e6-20",
+      source: "horizontal-6",
+      type: "smoothstep",
+      target: "horizontal-20",
       animated: true,
     },
     {
-      id: 'horizontal-e6-19',
-      source: 'horizontal-6',
-      type: 'smoothstep',
-      target: 'horizontal-19',
+      id: "horizontal-e6-19",
+      source: "horizontal-6",
+      type: "smoothstep",
+      target: "horizontal-19",
       animated: true,
     },
   ];
